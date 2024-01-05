11cdff6ac79ed3d557a961051b89113b953c6602
deadnotxaa <khudyakov26artem@gmail.com>
2024-01-05 19:47:53 +0300

    [feat] parsing and main functions implemented

--- lib/ArgParser.cpp
+++ lib/ArgParser.cpp
@@ -1,17 +1,77 @@
 #include "ArgParser.h"
 
 ArgumentParser::ArgParser::ArgParser(const char* value) {
-    this->parser_name_ = value;
+    parser_name_ = value;
 }
 
 bool ArgumentParser::ArgParser::Parse(int argc, char** argv) {
 
+    // Looks for help request
+    for (int i = 0; i < argc; ++i) {
+        if (!std::strcmp(argv[i], "-h") || !std::strcmp(argv[i], "--help")) {
+            // call help function
+        }
+    }
+
+    // Finds short parameters
+    for (int i = 0; i < argc; ++i) {
+        if (!std::strncmp(argv[i], "--", 2) || std::strncmp(argv[i], "-", 1) != 0) {
+            continue;
+        }
+
+        for (int j = 1; j < std::strlen(argv[i]); ++j) {
+            if (IsIntArgument(argv[i][j])) {
+                AddIntArgumentValue(argv[i][j], std::strtoll(argv[i + 1], nullptr, 10));
+            }
+            if (IsStringArgument(argv[i][j])) {
+                AddStringArgumentValue(argv[i][j], argv[i + 1]);
+            }
+            if (IsFlagArgument(argv[i][j])) {
+                AddFlagArgumentValue(argv[i][j], true);
+            }
+        }
+    }
+
+    // Finds long parameters
+    for (int i = 0; i < argc; ++i) {
+        if (std::strncmp(argv[i], "--", 2) != 0) {
+            continue;
+        }
+
+        const char* full_name = std::strtok(argv[i], "=");
+        const char* value = std::strtok(nullptr, "=");
+
+        if (IsIntArgument('\0', full_name)) {
+            AddIntArgumentValue(full_name, std::strtoll(value, nullptr, 10));
+        }
+        if (IsStringArgument('\0', full_name)) {
+            AddStringArgumentValue(full_name, value);
+        }
+        if (IsFlagArgument('\0', full_name)) {
+            AddFlagArgumentValue(full_name, !std::strcmp("true", value));
+        }
+    }
+
+    // Finds positional parameters
+    for (int i = 1; i < argc; ++i) {
+        if (!std::strncmp(argv[i], "--", 2) || !std::strncmp(argv[i], "-", 1)) {
+            continue;
+        }
+
+        if (IsIntPositionalArgument()) {
+            AddIntArgumentValue(GetPositionalIntArgumentName(), std::strtoll(argv[i], nullptr, 10));
+        } else {
+            AddStringArgumentValue(GetPositionalStringArgumentName(), argv[i]);
+        }
+    }
+
+    return CheckParserInput();
 }
 
 ArgumentParser::IntArgument& ArgumentParser::ArgParser::AddIntArgument(char short_name, const char* full_name, const char* description) {
     int_arguments_.emplace_back(IntArgument(short_name, full_name, description));
 
-    return *int_arguments_.end();
+    return int_arguments_[int_arguments_.size() - 1];
 }
 
 ArgumentParser::StringArgument& ArgumentParser::ArgParser::AddStringArgument(char short_name, const char* full_name, const char* description) {
@@ -26,6 +86,257 @@ ArgumentParser::FlagArgument& ArgumentParser::ArgParser::AddFlagArgument(char sh
     return *flag_arguments_.end();
 }
 
+bool ArgumentParser::ArgParser::IsIntArgument(char short_name, const char* full_name) {
+    if (full_name == nullptr) {
+        for (auto& i : int_arguments_) {
+            if (i.short_name_ == short_name) {
+                return true;
+            }
+        }
+    } else {
+        for (auto& i : int_arguments_) {
+            if (i.full_name_ == full_name) {
+                return true;
+            }
+        }
+    }
+
+    return false;
+}
+
+bool ArgumentParser::ArgParser::IsStringArgument(char short_name, const char* full_name) {
+    if (full_name == nullptr) {
+        for (auto& i : string_arguments_) {
+            if (i.short_name_ == short_name) {
+                return true;
+            }
+        }
+    } else {
+        for (auto& i : string_arguments_) {
+            if (i.full_name_ == full_name) {
+                return true;
+            }
+        }
+    }
+
+    return false;
+}
+
+bool ArgumentParser::ArgParser::IsFlagArgument(char short_name, const char* full_name) {
+    if (full_name == nullptr) {
+        for (auto& i : flag_arguments_) {
+            if (i.short_name_ == short_name) {
+                return true;
+            }
+        }
+    } else {
+        for (auto& i : flag_arguments_) {
+            if (i.full_name_ == full_name) {
+                return true;
+            }
+        }
+    }
+
+    return false;
+}
+
+void ArgumentParser::ArgParser::AddIntArgumentValue(char short_name, int64_t value) {
+    for (auto& i : int_arguments_) {
+        if (i.short_name_ == short_name) {
+            if (i.is_multi_value_) {
+                i.multi_value_storage_.emplace_back(value);
+                if (i.is_multi_value_external_storage_) {
+                    i.multi_value_external_storage_->emplace_back(value);
+                }
+            } else {
+                i.value_ = value;
+            }
+
+            if (i.is_external_storage_) {
+                *i.external_storage_ = value;
+            }
+
+            i.is_value_defined_ = true;
+            ++i.args_counter_;
+        }
+    }
+}
+
+void ArgumentParser::ArgParser::AddStringArgumentValue(char short_name, const char* value) {
+    for (auto& i : string_arguments_) {
+        if (i.short_name_ == short_name) {
+            if (i.is_multi_value_) {
+                i.multi_value_storage_.emplace_back(value);
+                if (i.is_multi_value_external_storage_) {
+                    i.multi_value_external_storage_->emplace_back(value);
+                }
+            } else {
+                i.value_ = value;
+            }
+
+            if (i.is_external_storage_) {
+                *i.external_storage_ = value;
+            }
+
+            i.is_value_defined_ = true;
+            ++i.args_counter_;
+        }
+    }
+}
+
+void ArgumentParser::ArgParser::AddFlagArgumentValue(char short_name, bool value) {
+    for (auto& i : flag_arguments_) {
+        if (i.short_name_ == short_name) {
+            if (i.is_multi_value_) {
+                i.multi_value_storage_.emplace_back(value);
+                if (i.is_multi_value_external_storage_) {
+                    i.multi_value_external_storage_->emplace_back(value);
+                }
+            } else {
+                i.value_ = value;
+            }
+
+            if (i.is_external_storage_) {
+                *i.external_storage_ = value;
+            }
+
+            i.is_value_defined_ = true;
+            ++i.args_counter_;
+        }
+    }
+}
+
+void ArgumentParser::ArgParser::AddIntArgumentValue(const char* full_name, int64_t value) {
+    for (auto& i : int_arguments_) {
+        if (i.full_name_ == full_name) {
+            if (i.is_multi_value_) {
+                i.multi_value_storage_.emplace_back(value);
+                if (i.is_multi_value_external_storage_) {
+                    i.multi_value_external_storage_->emplace_back(value);
+                }
+            } else {
+                i.value_ = value;
+            }
+
+            if (i.is_external_storage_) {
+                *i.external_storage_ = value;
+            }
+
+            i.is_value_defined_ = true;
+            ++i.args_counter_;
+        }
+    }
+}
+
+void ArgumentParser::ArgParser::AddStringArgumentValue(const char* full_name, const char* value) {
+    for (auto& i : string_arguments_) {
+        if (i.full_name_ == full_name) {
+            if (i.is_multi_value_) {
+                i.multi_value_storage_.emplace_back(value);
+                if (i.is_multi_value_external_storage_) {
+                    i.multi_value_external_storage_->emplace_back(value);
+                }
+            } else {
+                i.value_ = value;
+            }
+
+            if (i.is_external_storage_) {
+                *i.external_storage_ = value;
+            }
+
+            i.is_value_defined_ = true;
+            ++i.args_counter_;
+        }
+    }
+}
+
+void ArgumentParser::ArgParser::AddFlagArgumentValue(const char* full_name, bool value) {
+    for (auto& i : flag_arguments_) {
+        if (i.full_name_ == full_name) {
+            if (i.is_multi_value_) {
+                i.multi_value_storage_.emplace_back(value);
+                if (i.is_multi_value_external_storage_) {
+                    i.multi_value_external_storage_->emplace_back(value);
+                }
+            } else {
+                i.value_ = value;
+            }
+
+            if (i.is_external_storage_) {
+                *i.external_storage_ = value;
+            }
+
+            i.is_value_defined_ = true;
+            ++i.args_counter_;
+        }
+    }
+}
+
+const char *ArgumentParser::ArgParser::GetPositionalIntArgumentName() {
+    for (auto& i : int_arguments_) {
+        if (i.is_positional_) {
+            return i.full_name_;
+        }
+    }
+
+    return nullptr;
+}
+
+const char *ArgumentParser::ArgParser::GetPositionalStringArgumentName() {
+    for (auto& i : string_arguments_) {
+        if (i.is_positional_) {
+            return i.full_name_;
+        }
+    }
+
+    return nullptr;
+}
+
+bool ArgumentParser::ArgParser::IsIntPositionalArgument() {
+
+    return std::ranges::any_of(
+            int_arguments_.begin(),
+            int_arguments_.end(),
+            [](const IntArgument& i) {return i.is_positional_;});
+}
+
+bool ArgumentParser::ArgParser::CheckParserInput() {
+    for (auto& i : int_arguments_) {
+        if (!(i.is_value_defined_ || i.is_default_value_defined_)) {
+            return false;
+        }
+        if (i.is_multi_value_ && i.min_args_count_) {
+            if (i.args_counter_ < i.min_args_count_) {
+                return false;
+            }
+        }
+    }
+
+    for (auto& i : string_arguments_) {
+        if (!(i.is_value_defined_ || i.is_default_value_defined_)) {
+            return false;
+        }
+        if (i.is_multi_value_ && i.min_args_count_) {
+            if (i.args_counter_ < i.min_args_count_) {
+                return false;
+            }
+        }
+    }
+
+    for (auto& i : flag_arguments_) {
+        if (!(i.is_value_defined_ || i.is_default_value_defined_)) {
+            return false;
+        }
+        if (i.is_multi_value_ && i.min_args_count_) {
+            if (i.args_counter_ < i.min_args_count_) {
+                return false;
+            }
+        }
+    }
+
+    return true;
+}
+
 ArgumentParser::Argument::Argument(char short_name, const char* full_name, const char* description)
     :
     short_name_(short_name),
@@ -34,11 +345,16 @@ ArgumentParser::Argument::Argument(char short_name, const char* full_name, const
 {
 }
 
-ArgumentParser::Argument &ArgumentParser::Argument::Positional() {
+ArgumentParser::Argument &ArgumentParser::Argument::MultiValue(std::size_t value) {
+    min_args_count_ = value;
+    is_multi_value_ = true;
+
     return *this;
 }
 
-ArgumentParser::Argument &ArgumentParser::Argument::MultiValue(std::size_t) {
+ArgumentParser::Argument &ArgumentParser::Argument::Positional() {
+    this->is_positional_ = true;
+
     return *this;
 }
 
@@ -48,17 +364,23 @@ ArgumentParser::IntArgument::IntArgument(char short_name, const char* full_name,
 }
 
 ArgumentParser::IntArgument& ArgumentParser::IntArgument::Default(int64_t value) {
-    this->default_value_ = value;
-    this->is_default_value_defined_ = true;
+    default_value_ = value;
+    is_default_value_defined_ = true;
 
     return *this;
 }
 
-ArgumentParser::IntArgument &ArgumentParser::IntArgument::StoreValue() {
+ArgumentParser::IntArgument& ArgumentParser::IntArgument::StoreValue(int64_t& reference) {
+    external_storage_ = &reference;
+    is_external_storage_ = true;
+
     return *this;
 }
 
-ArgumentParser::IntArgument &ArgumentParser::IntArgument::StoreValues() {
+ArgumentParser::IntArgument &ArgumentParser::IntArgument::StoreValues(std::vector<int64_t>& reference) {
+    multi_value_external_storage_ = &reference;
+    is_multi_value_external_storage_ = true;
+
     return *this;
 }
 
@@ -68,17 +390,23 @@ ArgumentParser::StringArgument::StringArgument(char short_name, const char* full
 }
 
 ArgumentParser::StringArgument& ArgumentParser::StringArgument::Default(const char* value) {
-    this->default_value_ = value;
-    this->is_default_value_defined_ = true;
+    default_value_ = value;
+    is_default_value_defined_ = true;
 
     return *this;
 }
 
-ArgumentParser::StringArgument &ArgumentParser::StringArgument::StoreValue() {
+ArgumentParser::StringArgument& ArgumentParser::StringArgument::StoreValue(const char*& reference) {
+    external_storage_ = &reference;
+    is_external_storage_ = true;
+
     return *this;
 }
 
-ArgumentParser::StringArgument &ArgumentParser::StringArgument::StoreValues() {
+ArgumentParser::StringArgument& ArgumentParser::StringArgument::StoreValues(std::vector<const char*>& reference) {
+    multi_value_external_storage_ = &reference;
+    is_multi_value_external_storage_ = true;
+
     return *this;
 }
 
@@ -88,16 +416,22 @@ ArgumentParser::FlagArgument::FlagArgument(char short_name, const char* full_nam
 }
 
 ArgumentParser::FlagArgument& ArgumentParser::FlagArgument::Default(bool value) {
-    this->default_value_ = value;
-    this->is_default_value_defined_ = true;
+    default_value_ = value;
+    is_default_value_defined_ = true;
 
     return *this;
 }
 
-ArgumentParser::FlagArgument &ArgumentParser::FlagArgument::StoreValue() {
+ArgumentParser::FlagArgument& ArgumentParser::FlagArgument::StoreValue(bool& reference) {
+    external_storage_ = &reference;
+    is_external_storage_ = true;
+
     return *this;
 }
 
-ArgumentParser::FlagArgument &ArgumentParser::FlagArgument::StoreValues() {
+ArgumentParser::FlagArgument& ArgumentParser::FlagArgument::StoreValues(std::vector<bool>& reference) {
+    multi_value_external_storage_ = &reference;
+    is_multi_value_external_storage_ = true;
+
     return *this;
 }
--- lib/ArgParser.h
+++ lib/ArgParser.h
@@ -5,11 +5,15 @@
 
 #include <vector>
 #include <cinttypes>
+#include <cstring>
+#include <cctype>
+#include <algorithm>
 
 namespace ArgumentParser {
 
 class ArgParser;
 
+// Base class for arguments
 class Argument {
     friend ArgParser;
 
@@ -20,12 +24,19 @@ public:
 protected:
     Argument(char, const char*, const char*);
 
-    char short_name_;
-    const char* full_name_ = nullptr;
-    const char* description_ = nullptr;
+    char short_name_{};
+    const char* full_name_{};
+    const char* description_{};
 
+    std::size_t min_args_count_ = 0;
+    std::size_t args_counter_ = 0;
+
+    bool is_value_defined_ = false;
     bool is_default_value_defined_ = false;
     bool is_multi_value_ = false;
+    bool is_positional_ = false;
+    bool is_external_storage_ = false;
+    bool is_multi_value_external_storage_ = false;
 };
 
 
@@ -34,14 +45,17 @@ class IntArgument : public Argument {
 
 public:
     IntArgument& Default(int64_t);
-    IntArgument& StoreValue();
-    IntArgument& StoreValues();
+    IntArgument& StoreValue(int64_t&);
+    IntArgument& StoreValues(std::vector<int64_t>&);
 
 private:
     IntArgument(char, const char*, const char*);
 
-    int64_t value_;
-    int64_t default_value_;
+    int64_t value_{};
+    int64_t default_value_{};
+    int64_t* external_storage_{};
+    std::vector<int64_t> multi_value_storage_{};
+    std::vector<int64_t>* multi_value_external_storage_{};
 };
 
 
@@ -50,14 +64,18 @@ class StringArgument : public Argument {
 
 public:
     StringArgument& Default(const char*);
-    StringArgument& StoreValue();
-    StringArgument& StoreValues();
+    StringArgument& StoreValue(const char*&);
+    StringArgument& StoreValues(std::vector<const char*>&);
 
 private:
     StringArgument(char, const char*, const char*);
 
-    const char* value_;
-    const char* default_value_;
+    const char* value_{};
+    const char* default_value_{};
+
+    const char** external_storage_{};
+    std::vector<const char*> multi_value_storage_{};
+    std::vector<const char*>* multi_value_external_storage_{};
 };
 
 
@@ -66,14 +84,18 @@ class FlagArgument : public Argument {
 
 public:
     FlagArgument& Default(bool);
-    FlagArgument& StoreValue();
-    FlagArgument& StoreValues();
+    FlagArgument& StoreValue(bool&);
+    FlagArgument& StoreValues(std::vector<bool>&);
 
 private:
     FlagArgument(char, const char*, const char*);
 
-    bool value_;
-    bool default_value_;
+    bool value_{};
+    bool default_value_{};
+
+    bool* external_storage_{};
+    std::vector<bool> multi_value_storage_{};
+    std::vector<bool>* multi_value_external_storage_{};
 };
 
 
@@ -89,8 +111,26 @@ public:
     StringArgument& AddStringArgument(char, const char*, const char*);
     FlagArgument& AddFlagArgument(char, const char*, const char*);
 
+    bool IsIntArgument(char = '\0', const char* = nullptr);
+    bool IsStringArgument(char = '\0', const char* = nullptr);
+    bool IsFlagArgument(char = '\0', const char* = nullptr);
+
+    void AddIntArgumentValue(char, int64_t);
+    void AddStringArgumentValue(char, const char*);
+    void AddFlagArgumentValue(char, bool);
+
+    void AddIntArgumentValue(const char*, int64_t);
+    void AddStringArgumentValue(const char*, const char*);
+    void AddFlagArgumentValue(const char*, bool);
+
+    const char* GetPositionalIntArgumentName();
+    const char* GetPositionalStringArgumentName();
+
+    bool IsIntPositionalArgument();
+    bool CheckParserInput();
+
 private:
-    [[maybe_unused]] const char* parser_name_ = nullptr;
+    const char* parser_name_{};
 
     std::vector<IntArgument> int_arguments_;
     std::vector<StringArgument> string_arguments_;
